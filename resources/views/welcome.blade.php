c84340da057b39068b65ecac4f922176a5be0ee6
andrii <gazhur94@gmail.com>
2018-05-03 17:25:55 +0300

    added js, added head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,12 @@
-<link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<head>
+  <title>Blog</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+</head>
 
-<!-- <script src="https://use.fontawesome.com/1dec14be15.js"></script> -->
+<script src="js/icons.js"></script>
     <div class="container-fluid stylish-form">
       <h2 class="text-center">Stylish Signup Page Using Bootstrap</h2>
       <div class="row mar20" >
@@ -11,29 +14,17 @@
           <div class="inner-section">
             <form method="POST" action="https://google.co.in">
               <div class="mar20 inside-form">
-                <h2 class="font_white text-center">SIGN UP</h2>
-                <ul>
-                  <li class="icon-holder dsp-flex">
-                    <i class="fa fa-facebook "></i>
-                  </li>
-                  <li class="icon-holder dsp-flex">
-                    <i class="fa fa-twitter "></i>
-                  </li>
-                  <li class="icon-holder dsp-flex">
-                    <i class="fa fa-instagram "></i>
-                  </li>
-                </ul>
+                <h2 class="font_white text-center">Реєстрація</h2>
+                
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-pencil "></i></span>
-                  <input type="text" class="form-control" name="text" placeholder="First Name...">
+                  <input type="text" class="form-control" name="text" placeholder="Логін...">
                 </div>
-                <div class="input-group">
-                  <span class="input-group-addon"><i class="fa fa-envelope "></i></span>
-                  <input type="email" class="form-control" name="text" placeholder="Email...">
-                </div>
+               
+               
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-lock "></i></span>
-                  <input type="password" class="form-control" name="text" placeholder="Password...">
+                  <input type="password" class="form-control" name="text" placeholder="Пароль...">
                 </div>
                 <div class="footer text-center">
                   <a href="http://vijender.in/" class="btn btn-neutral btn-round btn-lg">Get Started</a>
